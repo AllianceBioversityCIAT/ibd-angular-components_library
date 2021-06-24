99dc6d084065cc8574e81a1078bfe39904a585f3
Yecksin <yecksin@gmail.com>
2021-06-24 11:53:51 -0500

    Organized project

--- projects/ibd-angular-components/src/lib/ibd-angular-components.module.ts
+++ projects/ibd-angular-components/src/lib/ibd-angular-components.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
+import { SelectComponent } from './form-elements/select/select.component';
 import { IbdAngularComponentsComponent } from './ibd-angular-components.component';
 
 
 
+const componentsList = [
+  IbdAngularComponentsComponent,
+  SelectComponent
+];
 @NgModule({
-  declarations: [IbdAngularComponentsComponent],
-  imports: [
-  ],
-  exports: [IbdAngularComponentsComponent]
+  declarations: [...componentsList],
+  exports: [...componentsList],
+  imports: []
+  
 })
 export class IbdAngularComponentsModule { }
