6bb4b5d6c72df1f0c2ac3e0402d5f4212c319000
Yecksin <yecksin@gmail.com>
2021-06-28 16:17:26 -0500

    Menu was created

--- src/app/shared/services/data-control.service.ts
+++ src/app/shared/services/data-control.service.ts
@@ -6,7 +6,28 @@ import { Injectable } from '@angular/core';
 export class DataControlService {
 
   constructor() { }
-
+  menuSections = [
+    {
+      name:'About',
+    },
+    {
+      name:'Getting Started',
+    },
+    {
+      name:'Components',
+      children:[
+        {
+          name:'save button'
+        },
+        {
+          name:'Select'
+        },
+        {
+          name:'TextArea'
+        }
+      ]
+    }
+  ]
   
 
 }
