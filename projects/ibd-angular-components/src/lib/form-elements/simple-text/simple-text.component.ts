22fcf49024598d45889d40d27d687bf6f1f4723e
Yecksin <yecksin@gmail.com>
2021-08-12 11:07:43 -0500

    SimpleTextComponent component was added

--- projects/ibd-angular-components/src/lib/form-elements/simple-text/simple-text.component.ts
+++ projects/ibd-angular-components/src/lib/form-elements/simple-text/simple-text.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { textOptions } from '../../models/text-options.interface';
 
 @Component({
   selector: 'ibdc-simple-text',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./simple-text.component.scss']
 })
 export class SimpleTextComponent implements OnInit {
-
+  @Input() options:textOptions;
   constructor() { }
 
   ngOnInit(): void {
