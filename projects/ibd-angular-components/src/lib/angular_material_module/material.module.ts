b1228f0f89ceaf6c6eaf78fff607ee9877eaced3
Yecksin Mauricio <yecksin@gmail.com>
2021-09-03 11:46:32 -0500

    Some material modules was removed

--- projects/ibd-angular-components/src/lib/angular_material_module/material.module.ts
+++ projects/ibd-angular-components/src/lib/angular_material_module/material.module.ts
@@ -1,10 +1,5 @@
 import { NgModule } from '@angular/core';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatListModule } from '@angular/material/list';
-import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
@@ -46,12 +41,7 @@ import { OverlayModule } from '@angular/cdk/overlay';
 
 
 const myModules = [
-  MatToolbarModule,
-  MatSidenavModule,
   MatButtonModule,
-  MatMenuModule,
-  MatListModule,
-  MatIconModule,
   MatInputModule,
   MatCardModule,
   MatTableModule,
