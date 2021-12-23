938e3aa3d091cc25236d9287a9bd1119c179cb26
Yecksin Mauricio <yecksin@gmail.com>
2021-12-23 17:07:18 -0500

    Input number corrrection

--- projects/ibd-angular-components/src/lib/form-elements/input/input.component.ts
+++ projects/ibd-angular-components/src/lib/form-elements/input/input.component.ts
@@ -36,32 +36,35 @@ export class InputComponent implements OnInit {
 
   setValueWithNumber(){
     this.options.form.controls[this.options.formControlName].setValue(this.numericField);
-    if (this.options.min === 0 && (this.numericField < this.options.min)) {
-      this.numericField = this.options.min;
-      this.options.form.controls[this.options.formControlName].setValue(this.options.min);
-      this.reloadInput = false;
-      setTimeout(() => {
-        this.reloadInput = true;
-      }, 10);
-    }
+    // if (this.options.min === 0 && (this.numericField < this.options.min)) {
+    //   this.numericField = this.options.min;
+    //   this.options.form.controls[this.options.formControlName].setValue(this.options.min);
+    //   this.reloadInput = false;
+    //   setTimeout(() => {
+    //     this.reloadInput = true;
+    //   }, 10);
+    //   console.log("A")
+    // }
 
-    if (this.options.max && (this.numericField > this.options.max)) {
-      this.numericField = this.options.max;
-      this.options.form.controls[this.options.formControlName].setValue(this.options.max);
-      this.reloadInput = false;
-      setTimeout(() => {
-        this.reloadInput = true;
-      }, 10);
-    }
+    // if (this.options.max && (this.numericField > this.options.max)) {
+    //   this.numericField = this.options.max;
+    //   this.options.form.controls[this.options.formControlName].setValue(this.options.max);
+    //   this.reloadInput = false;
+    //   setTimeout(() => {
+    //     this.reloadInput = true;
+    //   }, 10);
+    //   console.log("B")
+    // }
 
-    if (this.numericField == null) {
-      this.options.form.controls[this.options.formControlName].setValue(0);
-      this.numericField = 0;
-      this.reloadInput = false;
-      setTimeout(() => {
-        this.reloadInput = true;
-      }, 10);
-    }
+    // if (this.numericField === null) {
+    //   this.options.form.controls[this.options.formControlName].setValue(0);
+    //   this.numericField = 0;
+    //   this.reloadInput = false;
+    //   setTimeout(() => {
+    //     this.reloadInput = true;
+    //   }, 10);
+    //   console.log("C")
+    // }
 
   }
 
